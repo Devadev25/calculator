98e2d3cdc344a3ae0dd4e84bb1c00ad36b3db2cd
Agira-deva <deva.d@agiratech.com>
2024-01-02 15:02:48 +0530

    new changes

--- function.js
+++ function.js
@@ -1,20 +1,59 @@
-window.onload=()=>{
-    document.getElementById("display").focus()
+
+window.onload = () => {
+  document.getElementById("display").focus();
 }
+
 function clearInput() {
-    document.querySelector('input[name="answer"]').value = '';
-  }
+  document.querySelector('input').value = '';
+}
+
+function deleteLastChar() {
+  const currentInput = document.querySelector('input').value;
+  document.querySelector('input').value = currentInput.substr(0, currentInput.length - 1);
+}
+
+function appendToInput(value) {
+  document.querySelector('input').value += value;
+}
+
+function calculateResult() {
+  const currentInput = document.querySelector('input').value;
+  const result = customCalculate(currentInput);
+
   
-  function deleteLastChar() {
-    const currentInput = document.querySelector('input[name="answer"]').value;
-    document.querySelector('input[name="answer"]').value = currentInput.substr(0, currentInput.length - 1);
+  localStorage.setItem('result',result);
+   sessionStorage.setItem('result',result)
+
+  document.querySelector('input').value = result;
+}
+
+function customCalculate(input) {
+  try {
+    // Regular expression to match valid operators (+, -, *, /)
+    const operatorRegex = /[-+*/]/;
+    // Split the input into operands and operator
+    const [operand1, operator, operand2] = input.split(operatorRegex);
+    
+    // Convert operands to numbers
+    const num1 = parseInt(operand1);
+    const num2 = parseInt(operand2);
+
+    // Perform the calculation based on the operator
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        
+        return num1 / num2;
+      default:
+        throw new Error('Invalid operator');
+    }
+  } catch (error) {
+    return 'Error';
   }
-  
-  function appendToInput(value) {
-    document.querySelector('input[name="answer"]').value += value;
-  }
-  
-  function calculateResult() {
-    document.querySelector('input[name="answer"]').value = eval(document.querySelector('input[name="answer"]').value);
-  }
-  
+}
